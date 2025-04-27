5a3b0ce553f01d2e35310f9f98a05d249ce07223
Shimon-Tanaka <shimontk11165478@gmail.com>
2025-04-27 14:55:18 +0900

    Initial Commit

---
+++ sp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sp1
+//
+//  Created by 田中志門 on 4/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("時間割")
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sp1/sp1App.swift
@@ -0,0 +1,17 @@
+//
+//  sp1App.swift
+//  sp1
+//
+//  Created by 田中志門 on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct sp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
